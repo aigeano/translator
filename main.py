7931c152f6cbc53fbd284f7dd70d383c54ffbe04
aigeano <aigeano@gmail.com>
2014-07-16 18:01:33 +0530

    Added functionality for translating into ten languages

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import sys, requests, json
 def main():
 	
 	text = raw_input('Enter text to be translated\n')
-	url = "http://api.mymemory.translated.net/get?q="+text+"&langpair=en|it"
-	
-	
-	r = requests.get(url)
 
-        json_data = json.loads(r.text)
-	print "Translated text in Italian :"
-        print json_data['responseData']['translatedText']
+	lang = {'italian':'it','german':'de','russian':'ru','french':'fr','spanish':'es','swedish':'sv','dutch':'nl','greek':'el','hungarian':'hu','indonesian':'id'}
+        
+
+	url = "http://api.mymemory.translated.net/get?q="+text+"&langpair=en|"
+	
+        for key in lang.keys():
+
+	  r = requests.get(url+lang.get(key))
+
+          json_data = json.loads(r.text)
+	  print "Translated text in",key," :" 
+          print json_data['responseData']['translatedText']
 
 if __name__=='__main__':
   main()
